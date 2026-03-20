4d304182cbf02b82ffb0bc02bc23f66601ea8cce
Ian Bridgwood <ianb@ek.dk>
2026-03-20 09:06:47 +0100

    Insert initial profile data into data.sql

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO profiles (name, email) VALUES
+    ('Emma Jensen', 'emma.jensen@example.com'),
+    ('Noah Andersen', 'noah.andersen@example.com'),
+    ('Ida Nielsen', 'ida.nielsen@example.com'),
+    ('Oscar Pedersen', 'oscar.pedersen@example.com'),
+    ('Freja Hansen', 'freja.hansen@example.com');
